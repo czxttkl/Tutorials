2e9dd08afcf9dee787bdc2f32850d2f9191a988d
Zhengxing Chen <chenz@instagram.com>
2019-01-06 20:14:17 -0800

    add several experiments

--- experiments/try_list_all_openai_envs.py
+++ experiments/try_list_all_openai_envs.py
@@ -8,6 +8,8 @@ for d in envs.registry.all():
         env = gym.make(env_id)
         print("action space", env.action_space)
         print("observation space", env.observation_space)
+        if env_id == 'CarRacing-v0':
+            print('a')
     except:
         print("not successful")
     print()
