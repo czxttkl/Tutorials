9ae2df1d571111315003c6e47b1d93ad6828b44e
Zhengxing Chen <chenz@instagram.com>
2019-01-06 20:13:41 -0800

    add cartpole env

---
+++ action_state_generation/env/cartpole_env.py
@@ -0,0 +1,22 @@
+"""
+An open AI gym environment "CartPole-v0"
+State space is 4-dimensional continuous space. Action space is 2 discrete actions.
+"""
+import numpy as np
+import torch
+import gym
+from lunar_env import LunarEnv
+
+
+class CartPoleEnv(LunarEnv):
+
+    def __init__(self, device):
+        self.env = gym.make("MountainCar-v0")
+        self.device = device
+        self.action_dim = self.env.action_space.n
+        self.state_dim = self.env.observation_space.shape[0]
+        self.cur_state = self.env.reset()
+
+    def test_step_limit(self):
+        """ test step limit. If test steps beyond this limit, terminate the episode """
+        return 1001
