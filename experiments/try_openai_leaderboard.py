e9e79a1016628aa4c0709b887309e8bf75c0bb56
Zhengxing Chen <chenz@instagram.com>
2019-01-07 23:46:49 -0800

    openai leaderboard prepared to check why my dqn doesn't work

--- experiments/try_openai_leaderboard.py
+++ experiments/try_openai_leaderboard.py
@@ -6,13 +6,19 @@ from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+import os
+import sys
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../action_state_generation/model/')
+from dqn import DQN
 
 
 class DQNCartPoleSolver():
-    def __init__(self, n_episodes=20000, n_win_ticks=195, max_env_steps=None, gamma=1.0, epsilon=1.0, epsilon_min=0.01,
+    def __init__(self, n_episodes=20000, n_win_ticks=195, max_env_steps=None, gamma=1.0, epsilon=1.0, epsilon_min=0.2,
                  epsilon_log_decay=0.995, alpha=0.01, alpha_decay=0.01, batch_size=64, monitor=False, quiet=False):
         self.memory = deque(maxlen=100000)
         self.env = gym.make('CartPole-v0')
+        # self.env = gym.make('LunarLander-v2')
         if monitor: self.env = gym.wrappers.Monitor(self.env, '../data/cartpole-1', force=True)
         self.gamma = gamma
         self.epsilon = epsilon
@@ -28,10 +34,21 @@ class DQNCartPoleSolver():
 
         # Init model
         self.model = Sequential()
-        self.model.add(Dense(24, input_dim=self.env.observation_space.shape[0], activation='tanh'))
-        self.model.add(Dense(48, activation='tanh'))
+        self.model.add(Dense(100, input_dim=self.env.observation_space.shape[0], activation='tanh'))
+        self.model.add(Dense(100, activation='relu'))
         self.model.add(Dense(self.env.action_space.n, activation='linear'))
         self.model.compile(loss='mse', optimizer=Adam(lr=self.alpha, decay=self.alpha_decay))
+
+        # Init my DQN
+        dqn_input_dim = self.env.observation_space.shape[0]
+        dqn_hidden_dim = 100
+        dqn_num_layer = 2
+        replay_memory_size = 10000
+        dqn_output_dim = self.env.action_space.n
+        parametric = False
+        policy_net = DQN(dqn_input_dim, dqn_num_layer,
+                         dqn_hidden_dim, dqn_output_dim,
+                         parametric, gamma, replay_memory_size, batch_size)
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
@@ -44,7 +61,7 @@ class DQNCartPoleSolver():
         return max(self.epsilon_min, min(self.epsilon, 1.0 - math.log10((t + 1) * self.epsilon_decay)))
 
     def preprocess_state(self, state):
-        return np.reshape(state, [1, 4])
+        return np.reshape(state, [1, self.env.observation_space.shape[0]])
 
     def replay(self, batch_size):
         x_batch, y_batch = [], []
@@ -61,7 +78,8 @@ class DQNCartPoleSolver():
             self.epsilon *= self.epsilon_decay
 
     def run(self):
-        scores = deque(maxlen=100)
+        final_scores = deque(maxlen=100)
+        accu_scores = deque(maxlen=100)
 
         for e in range(self.n_episodes):
             state = self.preprocess_state(self.env.reset())
@@ -73,15 +91,17 @@ class DQNCartPoleSolver():
                 next_state = self.preprocess_state(next_state)
                 self.remember(state, action, reward, next_state, done)
                 state = next_state
-                i += 1
+                i += reward
 
-            scores.append(i)
-            mean_score = np.mean(scores)
-            if mean_score >= self.n_win_ticks and e >= 100:
-                if not self.quiet: print('Ran {} episodes. Solved after {} trials ✔'.format(e, e - 100))
-                return e - 100
+            final_scores.append(reward)
+            accu_scores.append(i)
+            mean_final_score = np.mean(final_scores)
+            mean_accu_score = np.mean(accu_scores)
+            # if mean_score >= self.n_win_ticks and e >= 100:
+            #     if not self.quiet: print('Ran {} episodes. Solved after {} trials ✔'.format(e, e - 100))
+            #     return e - 100
             if e % 100 == 0 and not self.quiet:
-                print('[Episode {}] - Mean survival time over last 100 episodes was {} ticks.'.format(e, mean_score))
+                print('[Episode {}] - Last 100 episodes final reward: {}, accu reward: {}.'.format(e, mean_final_score, mean_accu_score))
 
             self.replay(self.batch_size)
 
