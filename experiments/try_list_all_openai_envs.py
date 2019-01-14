a77db25c76185b6ccf713a7e0d2f1b969894e6a7
Zhengxing Chen <chenz@instagram.com>
2019-01-14 10:07:30 -0800

    check in world model reproduce code

--- experiments/try_list_all_openai_envs.py
+++ experiments/try_list_all_openai_envs.py
@@ -8,8 +8,11 @@ for d in envs.registry.all():
         env = gym.make(env_id)
         print("action space", env.action_space)
         print("observation space", env.observation_space)
-        if env_id == 'CarRacing-v0':
+        print("observation space shape", env.observation_space.shape)
+        if env_id == 'CartPole-v0':
             print('a')
+        if env.observation_space.shape is None:
+            print('b')
     except:
         print("not successful")
     print()
