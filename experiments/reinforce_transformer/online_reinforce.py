36f50622102b8c2496917dac709d8939f90468eb
Zhengxing Chen <chenz@instagram.com>
2019-08-24 12:44:25 -0700

    my reinforce work on small example

--- experiments/reinforce_transformer/online_reinforce.py
+++ experiments/reinforce_transformer/online_reinforce.py
@@ -9,8 +9,9 @@ from torch import optim
 from reinforce_transformer_classes import ReinforceLossCompute
 
 
-class policy_estimator():
+class PolicyEstimator(nn.Module):
     def __init__(self, env):
+        super(PolicyEstimator, self).__init__()
         self.n_inputs = env.observation_space.shape[0]
         self.n_outputs = env.action_space.n
 
@@ -21,9 +22,26 @@ class policy_estimator():
             nn.Linear(16, self.n_outputs),
             nn.Softmax(dim=-1))
 
-    def predict(self, state):
+    def forward(self, state):
         action_probs = self.network(torch.FloatTensor(state))
         return action_probs
+
+
+class Baseline(nn.Module):
+    def __init__(self, env):
+        super(Baseline, self).__init__()
+        self.n_inputs = env.observation_space.shape[0]
+        self.n_outputs = 1
+
+        # Define network
+        self.network = nn.Sequential(
+            nn.Linear(self.n_inputs, 16),
+            nn.ReLU(),
+            nn.Linear(16, self.n_outputs))
+
+    def forward(self, state):
+        baseline_reward = self.network(torch.FloatTensor(state))
+        return baseline_reward
 
 
 def discount_rewards(rewards, gamma=0.99):
@@ -45,8 +63,10 @@ def reinforce(env, policy_estimator, num_episodes=2000,
     batch_counter = 1
 
     # Define optimizer
-    optimizer = optim.Adam(policy_estimator.network.parameters(),
-                           lr=0.01)
+    baseline = Baseline(env)
+    optimizer = optim.Adam(policy_estimator.parameters(), lr=0.01)
+    baseline_optimizer = optim.Adam(baseline.parameters())
+    reinforce_trainer = ReinforceLossCompute(optimizer, baseline_optimizer)
 
     action_space = np.arange(env.action_space.n)
     for ep in range(num_episodes):
@@ -57,7 +77,7 @@ def reinforce(env, policy_estimator, num_episodes=2000,
         complete = False
         while complete == False:
             # Get actions and convert to numpy array
-            action_probs = policy_estimator.predict(s_0).detach().numpy()
+            action_probs = policy_estimator(s_0).detach().numpy()
             action = np.random.choice(action_space, p=action_probs)
             s_1, r, complete, _ = env.step(action)
 
@@ -74,31 +94,50 @@ def reinforce(env, policy_estimator, num_episodes=2000,
                 batch_counter += 1
                 total_rewards.append(sum(rewards))
 
+                # my reinforce
                 # If batch is complete, update network
                 if batch_counter == batch_size:
-                    optimizer.zero_grad()
                     state_tensor = torch.FloatTensor(batch_states)
                     reward_tensor = torch.FloatTensor(batch_rewards)
                     # Actions are used as indices, must be LongTensor
                     action_tensor = torch.LongTensor(batch_actions)
-
                     # Calculate loss
-                    logprob = torch.log(
-                        policy_estimator.predict(state_tensor))
-
-                    selected_logprobs = reward_tensor * \
-                                        logprob[np.arange(len(action_tensor)), action_tensor]
-                    loss = -selected_logprobs.mean()
-
-                    # Calculate gradients
-                    loss.backward()
-                    # Apply gradients
-                    optimizer.step()
+                    logprob = torch.log(policy_estimator(state_tensor))
+                    logprob = logprob[np.arange(len(action_tensor)), action_tensor]
+                    neg_reward_tensor = -reward_tensor
+                    reinforce_trainer(logprob, neg_reward_tensor, baseline, state_tensor)
 
                     batch_rewards = []
                     batch_actions = []
                     batch_states = []
                     batch_counter = 1
+
+                # original reinfoce
+                # If batch is complete, update network
+                # if batch_counter == batch_size:
+                #     optimizer.zero_grad()
+                #     state_tensor = torch.FloatTensor(batch_states)
+                #     reward_tensor = torch.FloatTensor(batch_rewards)
+                #     # Actions are used as indices, must be LongTensor
+                #     action_tensor = torch.LongTensor(batch_actions)
+                #
+                #     # Calculate loss
+                #     logprob = torch.log(
+                #         policy_estimator(state_tensor))
+                #
+                #     selected_logprobs = reward_tensor * \
+                #                         logprob[np.arange(len(action_tensor)), action_tensor]
+                #     loss = -selected_logprobs.mean()
+                #
+                #     # Calculate gradients
+                #     loss.backward()
+                #     # Apply gradients
+                #     optimizer.step()
+                #
+                #     batch_rewards = []
+                #     batch_actions = []
+                #     batch_states = []
+                #     batch_counter = 1
 
                 # Print running average
                 print("\rEp: {} Average of last 10: {:.2f}".format(
@@ -111,9 +150,9 @@ def reinforce(env, policy_estimator, num_episodes=2000,
 
 env = gym.make('CartPole-v0')
 s = env.reset()
-pe = policy_estimator(env)
-print(pe.predict(s))
-print(pe.network(torch.FloatTensor(s)))
+pe = PolicyEstimator(env)
+print(pe(s))
+print(pe(torch.FloatTensor(s)))
 
 rewards = reinforce(env, pe)
 window = 10
